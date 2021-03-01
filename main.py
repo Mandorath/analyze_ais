16a31cd664de0d9f0f6b67830a9c233c8694213a
Maurice <maurice@destroyer.of.worlds.com>
2021-03-01 14:53:29 +0100

    test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import fun_logger
 from extract import PreProcess
 from command_line import parseOptions
+from pro_yml import yaml_extract
 
 log = fun_logger.init_log()
 
@@ -13,6 +14,7 @@ if __name__ == '__main__':
     """Primary function."""
     parse_options = parseOptions()
     options = parse_options.arguments()
+    # Configure logging
     fun_logger.handle_log(log, options.LOG)
     fun_logger.handle_console(log)
 
@@ -22,4 +24,6 @@ if __name__ == '__main__':
     out_file = options.OUTPUT
     df = prepros.csv_to_df(csv_file)
     df = prepros.extract_rows_type(df, elem, column, True, out_file)
+    uniq = prepros.find_rows(df, 'MMSI')
+    print(uniq)
     # csv = file_h.read_csv(csv_file)
