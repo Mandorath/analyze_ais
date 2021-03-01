4de0adb563fffb47b67760ed76540d21ef831482
Maurice <maurice@destroyer.of.worlds.com>
2021-03-01 13:49:49 +0100

    Update docstrings

--- extract.py
+++ extract.py
@@ -15,22 +15,35 @@ class PreProcess():
         """
         Set the dict to a dataframe.
 
-        @file; string containing filename of the CSV file to read from.
+        Args:
+            file (str); Contains filename of the CSV file to read from.
         """
         df = pd.read_csv(file, index_col=None, header=0)
         return df
+
+    def find_rows(self, df, elem, column):
+        """
+        Find rows that matche string @elem in column @column.
+
+        Args:
+            elem (str): String to match when searching rows.
+            column (str): Contains name of the column to match the string.
+            df (df): Pandas dataframe
+        """
+
 
     def extract_rows_type(self, df, elem, column, remove, file_name):
         """
         Extract all rows based on value 'elem' in column 'column'.
 
-        @elem: String that matches with a value in specified column.
-        @column: Column that the value 'elem' should be matched.
-        @df: Pandas dataframe
-        @remove: Boolean, if true remove the rows after extraction from main
-                 pandas dataframe.
-        @file_name: The file the extracted rows should be written to.
-
+        Args:
+           elem (str): String, that matches with a value in specified column.
+           column (str): Contains name of the column that the value 'elem'
+                         should be matched.
+           df: Pandas dataframe
+           remove (bool): If true remove the rows from the dataframe
+                          after extraction from main pandas dataframe.
+           file_name (str): The file the extracted rows should be written to.
         """
         log.info("Extracting rows with value {0} from column {1}".format(elem, column))
         df_base = df[df[column] == elem]
@@ -43,10 +56,12 @@ class PreProcess():
         """
         Remove the rows from the main pandas dataframe.
 
-        @df: Pandas Dataframe
-        @elem: String in rows that is matched for removel
-        @column: String containing the column in which 'elem' should
-                 be matched.
+        Args:
+          df (df): Pandas Dataframe
+          elem (str): String used in rows that is matched for removal
+          column (str): Contains the column in which 'elem' should
+                        be matched.
+
         """
         log.info("Removing rows with value {0} from column {1}".format(elem, column))
         df = df[df[column] == elem]
@@ -56,8 +71,9 @@ class PreProcess():
         """
         Write Pandas dataframe to CSV file.
 
-        @df: Pandas Dataframe
-        @filename: String containing file name the dataframe should be
-                   written to.
+        Args:
+            df: Pandas Dataframe
+            filename: Contains file name the dataframe should be written to.
+
         """
         df.to_csv(filename)
