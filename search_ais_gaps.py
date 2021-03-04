7c5bfa19131f0337ebe329462d5f3e4d8ce7b2c5
Maurice <maurice@destroyer.of.worlds.com>
2021-03-04 12:53:30 +0100

    update search_ais_Gaps.py

--- search_ais_gaps.py
+++ search_ais_gaps.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 import pandas as pd
+from extract import PreProcess
+
+prepros = PreProcess()
+
 
 fmt = '%Y-%m-%d %H:%M:%S'
-timestamp_1 = datetime.strptime(row1[Timestamp], fmt)
-timestamp_2 = datetime.strptime(row2[Timestamp], fmt)
 
 
 def search_ais_gaps(minutes):
+    timestamp_1 = datetime.strptime(row1[Timestamp], fmt)
+    timestamp_2 = datetime.strptime(row2[Timestamp], fmt)
     time_delta = (timestamp_2 - timestamp_1)
     total_seconds = time_delta.total_seconds()
     minutes = total_seconds/60
@@ -14,6 +18,7 @@ def search_ais_gaps(minutes):
 
 
 def  search_mmsi(df, elem, uniq):
+    """Extract all rows from a given MMSI"""
     for val in uniq:
         df_rows_mmsi = prepros.find_rows(df, val, 'MMSI')
         search_ais_gaps(df_rows_mmsi)
