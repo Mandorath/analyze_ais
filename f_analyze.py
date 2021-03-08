448c546c3ca6f2a0273448f0a59b1a648c9afd01
mouw <maurice.mouw@saabgroup.com>
2021-03-08 14:59:46 +0100

    test this

--- f_analyze.py
+++ f_analyze.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas as pd
+import numpy as np
 from extract import PreProcess
 
 prepros = PreProcess()
@@ -8,12 +9,13 @@ prepros = PreProcess()
 fmt = '%Y-%m-%d %H:%M:%S'
 
 
-def search_ais_gaps(minutes):
-    timestamp_1 = datetime.strptime(row1[Timestamp], fmt)
-    timestamp_2 = datetime.strptime(row2[Timestamp], fmt)
-    time_delta = (timestamp_2 - timestamp_1)
-    total_seconds = time_delta.total_seconds()
-    minutes = total_seconds/60
+def search_ais_gaps(df, time):
+    minutes = df["# Timestamp"].diff().apply(lambda x: x/np.timedelta64(1, 'm')).fillna(0).astype('int64')
+    # timestamp_1 = datetime.strptime(row1[Timestamp], fmt)
+    # timestamp_2 = datetime.strptime(row2[Timestamp], fmt)
+    # time_delta = (timestamp_2 - timestamp_1)
+    # total_seconds = time_delta.total_seconds()
+    # minutes = total_seconds/60
     return minutes
 
 
@@ -24,6 +26,6 @@ def  search_mmsi(df, elem, uniq):
         search_ais_gaps(df_rows_mmsi)
 
 
-df["Flag"] = "No Problem"
-df.loc[df["minutes"] >= 3, "Flag"] = "Problem"
-df.to_csv("flag_{0}".format(filename))
+# df["Flag"] = "No Problem"
+# df.loc[df["minutes"] >= 3, "Flag"] = "Problem"
+# df.to_csv("flag_{0}".format(filename))
--- main.py
+++ main.py
@@ -4,15 +4,30 @@ import fun_logger
 import ruamel.yaml
 import pandas as pd
 import multiprocessing
+import numpy as np
 from extract import PreProcess
 from command_line import parseOptions
 from pro_yml import yaml_extract
+from f_analyze import search_ais_gaps
 # from search_ais_gaps import search_ais_gaps
 
 log = fun_logger.init_log()
 yml_tst = ruamel.yaml
 df_ships = pd.DataFrame()
 df_ship_type = pd.DataFrame()
+
+
+def parallelize_dataframe(df, func, n_cores=8):
+    df_split = np.array_split(df, n_cores)
+
+    pool = multiprocessing.Pool(n_cores)
+    df = pd.concat(pool.map(func, df_split))
+
+    pool.close()
+    pool.join()
+
+    return df
+
 
 def extract_classes(extract, df):
     print(yml_tst.safe_dump(extract))
@@ -27,31 +42,50 @@ def extract_classes(extract, df):
 
 
 def extract_vessel_types(extract, df, df_ships):
+    """
+    Extracts the vessel types specified in the yaml file.
+
+    Args:
+       extract (dict): Yaml file, that contains instructions.
+       df (df): Dataframe containing the data to extract parts of.
+       df_ships: Dataframe that the extracted data will be put in.
+
+    """
     print(yml_tst.safe_dump(extract))
     elem = extract['elem']
     column = extract['column']
     out_file = extract['out_file']
     rem_df = extract['remove_in_df']
     unique_col = extract['unique_column']
-    return_dict = {}
+    log.info("Extracting rows containing a ship type based on vessel type {0}".format(elem))
     df_base = prepros.extract_rows_type(df, elem, column)
+    log.info("Extracting unique values from the column {0}".format(unique_col))
     l_unique = prepros.extract_uniq_val(df_base, unique_col)
-    manager = multiprocessing.Manager()
     jobs = []
     for uni_val in l_unique:
-        return_dict = manager.dict()
-        pr = multiprocessing.Process(target=prepros.extract_unique_rows_type,
-                                     args=(df, uni_val, unique_col,
-                                           return_dict))
-        jobs.append(pr)
-        pr.start()
-        df_ship_type = return_dict
+        log.info("Extracting all rows matching MMSI {0}".format(uni_val))
+        df_ship_type = prepros.extract_rows_type(df, uni_val, unique_col)
         print(df_ship_type)
-        #df_ships = pd.concat([df_ships, df_ship_type])
+        log.info("Adding vessel with MMSI {0} dataframe to vessel_type dataframe".format(uni_val))
+        df_ships = pd.concat([df_ships, df_ship_type])
         # df = prepros.remove_rows(df, uni_val, unique_col)
         # prepros.csv_out(df, "remaining.out")
-    # prepros.csv_out(df_ships, out_file)
+    prepros.csv_out(df_ships, out_file)
 
+
+def analyze_vessels(extract, df, df_ships):
+    elem = extract['elem']
+    column = extract['column']
+    out_file = extract['out_file']
+    rem_df = extract['remove_in_df']
+    unique_col = extract['unique_column']
+    log.info("Extracting unique values from the column {0}".format(unique_col))
+    l_unique = prepros.extract_uniq_val(df, unique_col)
+    time = 6
+    for uni_val in l_unique:
+        df_ship_type = prepros.extract_rows_type(df, uni_val, unique_col)
+        minutes = search_ais_gaps(df_ship_type, time)
+        print(minutes)
 
 if __name__ == '__main__':
     """Primary function."""
@@ -84,7 +118,11 @@ if __name__ == '__main__':
             p = multiprocessing.Process(target=extract_vessel_types,
                                         args=(extract, df, df_ships,))
             p.start()
-
+    if 'analyze' in instruct:
+        for extract in instruct['extract_vessel_types']:
+            p = multiprocessing.Process(target=analyze_vessels,
+                                        args=(extract, df, df_ships,))
+            p.start()
 
 
             # prepros.csv_out(df_unique, out_file)
