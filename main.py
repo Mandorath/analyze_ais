d8374e0bceaceff7b599d04bd946b7dce9ee779e
mouw <maurice.mouw@saabgroup.com>
2021-03-11 15:41:58 +0100

    Add new analysis functions to main file and drop column mechanism.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 from extract import PreProcess
 from command_line import parseOptions
 from pro_yml import yaml_extract
-from f_analyze import search_ais_gaps
+from f_analyze import AnalyzeDf
 # from search_ais_gaps import search_ais_gaps
 
 log = fun_logger.init_log()
@@ -17,7 +17,7 @@ df_ships = pd.DataFrame()
 df_ship_type = pd.DataFrame()
 
 
-def parallelize_dataframe(df, func, n_cores=8):
+def parallelize_dataframe(df, func, n_cores=14):
     df_split = np.array_split(df, n_cores)
 
     pool = multiprocessing.Pool(n_cores)
@@ -30,6 +30,14 @@ def parallelize_dataframe(df, func, n_cores=8):
 
 
 def extract_classes(extract, df):
+    """
+    Extracts the classes specified in the yaml file.
+
+    Args:
+       extract (dict): Yaml file, that contains instructions.
+       df (df): Dataframe containing the data to extract parts of.
+
+    """
     print(yml_tst.safe_dump(extract))
     elem = extract['elem']
     column = extract['column']
@@ -74,24 +82,32 @@ def extract_vessel_types(extract, df, df_ships):
 
 
 def analyze_vessels(extract, df, df_ships):
-    elem = extract['elem']
-    column = extract['column']
     out_file = extract['out_file']
     rem_df = extract['remove_in_df']
     unique_col = extract['unique_column']
+    ais_gap = extract['ais_gap']
+    l_win = extract['l_bound_win']
+    u_win = extract['u_bound_win']
+    w_size = extract['window_size']
+    s_column = extract['speed_column']
+    t_column = extract['time_column']
     log.info("Extracting unique values from the column {0}".format(unique_col))
     l_unique = prepros.extract_uniq_val(df, unique_col)
-    time = 6
     for uni_val in l_unique:
         df_ship_type = prepros.extract_rows_type(df, uni_val, unique_col)
-        minutes = search_ais_gaps(df_ship_type, time)
-        print(minutes)
+        df_ship_type = analyze.search_ais_gaps(df_ship_type, t_column, ais_gap)
+        df_ship_type = analyze.grad_change(df_ship_type, s_column, w_size)
+        df_ship_type = analyze.perc_change_incr(df_ship_type, l_win, u_win)
+        df_ship_type = analyze.perc_change_decr(df_ship_type, l_win, u_win)
+        df_ships = pd.concat([df_ships, df_ship_type], ignore_index=True)
+    prepros.csv_out(df_ships, out_file)
 
 if __name__ == '__main__':
     """Primary function."""
     parse_options = parseOptions()
     options = parse_options.arguments()
     prepros = PreProcess()
+    analyze = AnalyzeDf()
     yml = yaml_extract()
     # Configure logging
     fun_logger.handle_log(log, options.LOG)
@@ -107,6 +123,9 @@ if __name__ == '__main__':
     df = prepros.csv_to_df(csv_file)
     # For all in extract block extract data.
     processes = []
+    if 'drop_columns' in instruct:
+        l_columns = instruct['drop_columns']
+        df = prepros.drop_col(df, l_columns)
     if 'extract' in instruct:
         for extract in instruct['extract']:
             p = multiprocessing.Process(target=extract_classes,
