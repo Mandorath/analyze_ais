f70ca322b9e8fab5529897a8435d299beb6575cc
mouw <maurice.mouw@saabgroup.com>
2021-03-12 08:40:47 +0100

    try this

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def prep_analysis(extract, df, l_poly):
         proc.start()
     for p in jobs:
         p.join()
-        p.close()
+        p.terminate()
     print(return_dict.values())
     # df_tot = pd.concat(res)
     # prepros.csv_out(df_tot, out_file)
