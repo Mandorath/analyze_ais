8d45c5b3744f74382ae33a602b582598635a5062
mouw <maurice.mouw@saabgroup.com>
2021-03-17 07:43:07 +0100

    wait for process

--- run_multiple_analyze.sh
+++ run_multiple_analyze.sh
@@ -4,8 +4,13 @@ dates=("20190701" "20190704")
 
 for i in ${dates[@]};
 do
-  python main.py --csv /home/maurice/aisdk_${i}/classA.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_cargo.log --yaml-file analyze_vessel_cargo.yaml
-  python main.py --csv /home/maurice/aisdk_${i}/classA.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_fishing.log --yaml-file analyze_vessel_fishing.yaml
-  python main.py --csv /home/maurice/aisdk_${i}/classA.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_passenger.log --yaml-file analyze_vessel_passenger.yaml
-  python main.py --csv /home/maurice/aisdk_${i}/classA.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_tanker.log --yaml-file analyze_vessel_tanker.yaml
+  python main.py --csv /home/maurice/aisdk_${i}/classA.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_cargo.log --yaml-file analyze_vessel_cargo.yaml &
+  P1=$!
+  python main.py --csv /home/maurice/aisdk_${i}/classA.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_fishing.log --yaml-file analyze_vessel_fishing.yaml &
+  P2=$!
+  python main.py --csv /home/maurice/aisdk_${i}/classA.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_passenger.log --yaml-file analyze_vessel_passenger.yaml &
+  P3=$!
+  python main.py --csv /home/maurice/aisdk_${i}/classA.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_tanker.log --yaml-file analyze_vessel_tanker.yaml &
+  P4=$!
+  wait $P1 $P2 $P3 $P4
 done
