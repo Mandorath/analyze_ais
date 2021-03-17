f8ac159f6a6650011717a8155e0e765d30dffd2d
mouw <maurice.mouw@saabgroup.com>
2021-03-17 07:31:07 +0100

    updated scripts

---
+++ run_multiple_analyze.sh
@@ -0,0 +1,8 @@
+
+
+dates=("20200701 20200702")
+
+for i in ${!dates[@]};
+do
+  python main.py --csv /home/maurice/aisdk_${i}/classA.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_cargo.log --yaml-file extract_class_a_vessel_type.yaml
+done
