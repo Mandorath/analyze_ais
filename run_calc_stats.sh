71ade447b1442087d8c0e54c13803f3ac9b8c3bc
mouw <maurice.mouw@saabgroup.com>
2021-03-18 13:14:20 +0100

    update shell script

--- run_calc_stats.sh
+++ run_calc_stats.sh
@@ -4,13 +4,13 @@ dates=("20190701" "20190704")
 
 for i in ${dates[@]};
 do
-  python main.py --csv /home/maurice/aisdk_${i}/Cargo2.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_cargo.log --yaml-file calc_stats_cargo.yaml &
+  python main.py --csv /home/maurice/aisdk_${i}/Cargo2.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_cargo_stats.log --yaml-file calc_stats_cargo.yaml --date ${i} &
   P1=$!
-  python main.py --csv /home/maurice/aisdk_${i}/Fishing2.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_fishing.log --yaml-file calc_stats_fishing.yaml &
+  python main.py --csv /home/maurice/aisdk_${i}/Fishing2.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_fishing_stats.log --yaml-file calc_stats_fishing.yaml --date ${i} &
   P2=$!
-  python main.py --csv /home/maurice/aisdk_${i}/Tanker2.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_passenger.log --yaml-file calc_stats_passenger.yaml &
+  python main.py --csv /home/maurice/aisdk_${i}/Tanker2.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_passenger_stats.log --yaml-file calc_stats_passenger.yaml --date ${i} &
   P3=$!
-  python main.py --csv /home/maurice/aisdk_${i}/Passenger2.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_tanker.log --yaml-file calc_stats_tanker.yaml &
+  python main.py --csv /home/maurice/aisdk_${i}/Passenger2.out --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_tanker_stats.log --yaml-file calc_stats_tanker.yaml  --date ${i}&
   P4=$!
   wait $P1 $P2 $P3 $P4
 done
