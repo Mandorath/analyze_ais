20b98be8466e6e008880a48191a753dbba5ef057
mouw <maurice.mouw@saabgroup.com>
2021-03-18 06:48:24 +0100

    extract mobile template script

---
+++ run_multiple_extract_mobile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+dates=("20200701 20200702")
+
+for i in ${dates[@]};
+do
+  python main.py --csv /AIS/AIS/Denmark/aisdk_${i}.csv --output-dir /home/maurice/aisdk_${i} --log-file aisdk_${i}_mobile.log --yaml-file extract_type_of_mobile.yaml
+done
